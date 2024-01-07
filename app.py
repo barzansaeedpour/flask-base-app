b305f87ae8501d3f0f50540c33b2dee94055a579
barzansaeedpour <barzansaeedpour@gmail.com>
2024-01-07 12:14:59 +0330

    freezing the requirements and ready to deploy

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def update(id):
         return render_template('update.html', task= task_to_update)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True) 
+    app.run(debug=False, host='0.0.0.0')
     with app.app_context():
         db.create_all()
